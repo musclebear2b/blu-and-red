8ba797d789cea0a966d1035211b3bd64debd8db3
Richard Whent <richardwhent@Richards-MacBook-Air.local>
2016-05-01 22:12:58 +0100

    added blue and red app

--- Hide the Red and Blue/ViewController.swift
+++ Hide the Red and Blue/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var blueBall: UIImageView!
+    @IBOutlet var redBall: UIImageView!
+    @IBOutlet var blueButton: UIButton!
+    @IBOutlet var redButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideTheBlueBall(sender: AnyObject) {
+        if blueBall.hidden == false {
+            blueBall.hidden = true
+            blueButton.setTitle("Show the Blue Ball"
+, forState: .Normal)
+        }  else  {
+            blueBall.hidden = false
+            blueButton.setTitle("Hide the Blue Ball"
+                , forState: .Normal)
+        }
+    }
 
+    @IBAction func hideTheRedBall(sender: AnyObject) {
+        if redBall.hidden == false {
+            redBall.hidden = true
+            redButton.setTitle("Show the Red Ball"
+                , forState: .Normal)
+        }  else  {
+            redBall.hidden = false
+            redButton.setTitle("Hide the Red Ball"
+                , forState: .Normal)
+        }
+    }
 }
 
